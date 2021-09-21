a319b62f302165fa2f1f9fe98d7751442e1a37d7
hermelio <hermelio.ifma@gmail.com>
2021-09-21 20:31:42 -0300

    versao 0

--- telas/views.py
+++ telas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    template_name = 'crud/home.html'
+    return render(request, template_name)
+
+def cadastro(request):
+    template_name = 'crud/cadastro.html'
+    return render(request, template_name)
