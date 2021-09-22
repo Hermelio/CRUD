e0debd1369ea04fdde7b4418e16b2499ebe2f3de
am1701 <djangosimples@gmail.com>
2021-09-22 06:43:14 -0300

    version 1.3

--- static/js/main.js
+++ static/js/main.js
@@ -21,4 +21,54 @@ $(".btn-salvar").on("click", function(e){
   });
 });
 
+
+
+
+
+function validaCpf(cpf) {
+
+	if (cpf.value.length !== 11 || cpf.value == '11111111111' || cpf.value == '22222222222' || cpf == '33333333333' ||
+		cpf.value == '44444444444' || cpf.value == '55555555555' || cpf.value == '66666666666' ||
+		cpf.value == '77777777777' || cpf.value == '88888888888' || cpf.value == '99999999999' ||
+		cpf.value == '01234567890') {
+
+		return false
+	}
+
+	resultado = 0;
+
+
+	for (x = 0, contador = 10; contador >1; x++, contador--) {
+
+		resultado += parseInt(cpf.value[x]) * contador;
+	}
+
+
+	digito_um = (resultado * 10) % 11
+
+
+	resultado = 0;
+
+	if (digito_um == parseInt(cpf.value[9])) {
+
+	for (x = 0, contador=11; contador>1; x++,contador--) {
+		resultado += parseInt(cpf.value[x]) * contador;
+	}
+
+	digito_dois = (resultado * 10) % 11;
+
+	if (digito_dois == cpf.value[10]) {
+
+		return true
+
+	} else {
+
+			return false
+		}
+	}
+
+
+}
+
+
 })
