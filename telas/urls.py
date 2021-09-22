8505c08656c924cb78d7e071c147d88ba7447155
am1701 <djangosimples@gmail.com>
2021-09-22 04:51:22 -0300

    algumas modificações

--- telas/urls.py
+++ telas/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 
+
+app_name='telas'
 urlpatterns = [
     path('', views.home, name='home'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    
+
 
 ]
